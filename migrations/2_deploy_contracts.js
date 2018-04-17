3510a766526ecfaf92d3fdee4fa979847890254f
rappazTJ <38465896+rappazTJ@users.noreply.github.com>
2018-04-17 22:47:18 +0900

    Update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,4 +1,4 @@
-var PresaleFirst = artifacts.require("./PresaleFirst.sol");
+var Presale = artifacts.require("./Presale.sol");
 var ProxyWallet = artifacts.require("./ProxyWallet.sol");
 
 module.exports = function(deployer) {
@@ -6,14 +6,14 @@ module.exports = function(deployer) {
   const endTime = startTime + (86400 * 20)
   const rate = new web3.BigNumber(2200)
   const wallet = web3.eth.accounts[0]
-  const cap = 1000000000
+  const cap = 100000000
   const minimum = 3000
 
   console.log(startTime)
   console.log(endTime)
 
-  deployer.deploy(PresaleFirst, startTime, endTime, rate, wallet, cap, minimum)
+  deployer.deploy(Presale, startTime, endTime, rate, wallet, cap, minimum)
     .then(function() {
-      return deployer.deploy(ProxyWallet, PresaleFirst.address)
+      return deployer.deploy(ProxyWallet, Presale.address)
     })
 };
